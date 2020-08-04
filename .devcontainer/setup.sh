9751ba08c2bdb16f1ce64897af1715321e018c01
Kristoffer-Andre Kalliainen <Kristoffer-Andre.Kalliainen@stacc.com>
2020-08-04 21:12:59 +0000

    Install make setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,6 @@
 # install curl, git, ...
 apt-get update
-apt-get install -y curl git jq
+apt-get install -y curl git jq make
 
 useradd -m user
 su user
@@ -38,3 +38,4 @@ go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
 go get -u -v github.com/zmb3/gogetdoc
 go get -u -v golang.org/x/lint/golint
 go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v golang.org/x/tools/gopls
